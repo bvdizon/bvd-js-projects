96b59834d3e9b33fa6d7cac778cbe872318258c3
briandizon <bvdizon@gmail.com>
2020-09-20 02:56:21 +0800

    recipes project - added search functionality

--- recipes/js/app.js
+++ recipes/js/app.js
@@ -6,57 +6,64 @@ let porkRecipes = db.collection('pork-recipes');
 porkRecipes.orderBy('Title').onSnapshot(snapshot => {
   let changes = snapshot.docChanges();
   changes.forEach(change => {
-    if(change.type == "added") {
+    if (change.type == "added") {
       // call to function to insert or add HTML to UI
       addHTML(change.doc);
-    } 
+    }
   })
 });
 
 function addHTML(doc) {
-    let title = doc.data().Title;
-    let description = doc.data().Description;
-    let docID = doc.id;
-    let date = doc.data().Date;
-    let imageURL; 
-    doc.data().ImageURL ? imageURL = `images/${doc.data().ImageURL}` : imageURL = "http://placehold.it/500x325";
+  let title = doc.data().Title;
+  let description = doc.data().Description;
+  let docID = doc.id;
+  let date = doc.data().Date;
+  let imageURL;
+  doc.data().ImageURL ? imageURL = `images/${doc.data().ImageURL}` : imageURL = "http://placehold.it/500x325";
 
-    let html = `
-    <div class="col-lg-4 col-md-6 mb-4" data-id="${docID}">
+  let html = `    
       <div class="card h-100">
-        <img class="card-img-top" src="${imageURL}" alt="Check out the recipe for ${title}.">
-        <div class="card-body">
-          <h4 class="card-title">${title}</h4>
-          <p class="card-text">${description}</p>
-          <p class="card-text">${date.toDate().toDateString()}</p>
-        </div>
-        <div class="card-footer">
-          <a href="#" class="btn btn-danger delRecipe">Delete Recipe!</a>
-        </div>
-      </div>
-    </div>`;
-    displayGrid.insertAdjacentHTML('afterbegin', html);
+          <img class="card-img-top" src="${imageURL}" alt="Check out the recipe for ${title}.">
+          <div class="card-body">
+            <h4 class="card-title">${title}</h4>
+            <p class="card-text">${description}</p>
+            <p class="card-text">${date.toDate().toDateString()}</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-danger delRecipe">Delete Recipe!</a>
+          </div>
+      </div>`;
+  // creating div container
+  let div = document.createElement('div');
+  // adding classes
+  div.className = "col-lg-4 col-md-6 mb-4";
+  // adding specific attributes
+  div.setAttribute('data-id', docID);
+  // insert template lieterals to new element
+  div.innerHTML = html;
+  // appending to the parent element
+  displayGrid.appendChild(div);
 };
 
 
 addRecipeform.addEventListener('submit', (e) => {
   // prevents browser default (page refresh) when submit 
-  e.preventDefault(); 
+  e.preventDefault();
 
   // getting the values from the input fields of the form
   let recipeTitle = addRecipeform.recipeTitle.value;
   let recipeDescription = addRecipeform.recipeDescription.value;
   let addDate = new Date();
-  
-  if(recipeTitle, recipeDescription) {
-    porkRecipes.add({
-        Title: recipeTitle,
-        Description: recipeDescription,
-        Date: addDate
-      });
 
-      let shownElements = document.querySelectorAll('.show');
-      Array.from(shownElements).forEach(shown => shown.classList.remove('show'));
+  if (recipeTitle, recipeDescription) {
+    porkRecipes.add({
+      Title: recipeTitle,
+      Description: recipeDescription,
+      Date: addDate
+    });
+
+    let shownElements = document.querySelectorAll('.show');
+    Array.from(shownElements).forEach(shown => shown.classList.remove('show'));
   };
 
   addRecipeform.reset();
@@ -65,7 +72,7 @@ addRecipeform.addEventListener('submit', (e) => {
 
 displayGrid.addEventListener('click', e => {
   // check if element clicked is the Delete Recipe button, and alert to confirm
-  if(e.target.classList.contains('delRecipe') && confirm('Are you sure you want to delete this recipe?')) {
+  if (e.target.classList.contains('delRecipe') && confirm('Are you sure you want to delete this recipe?')) {
     // getting the document ID of the image grid to delete
 
     let docID = e.target.parentElement.parentElement.parentElement.getAttribute('data-id');
@@ -75,4 +82,22 @@ displayGrid.addEventListener('click', e => {
     // removing the recipe from the UI
     e.target.parentElement.parentElement.parentElement.remove();
   }
+});
+
+
+
+/* Search / Filter
+=======================*/
+const search = document.getElementById('search');
+const recipeTitles = document.querySelectorAll('.card-title');
+
+search.addEventListener('keyup', e => {
+  Array.from(displayGrid.children)
+    .filter(recipe => !recipe.textContent.toLowerCase().includes(search.value.trim().toLowerCase()))
+    .forEach(recipe => recipe.style.display = 'none');
+
+  Array.from(displayGrid.children)
+    .filter(recipe => recipe.textContent.toLocaleLowerCase().includes(search.value.trim().toLowerCase()))
+    .forEach(recipe => recipe.style.display = 'block');
+
 });
